b436a29a065b6670fdddc31c8a80cdf12c35ac10
Shubham Singhal <sbmsgl1129@gmail.com>
2020-05-03 13:02:04 -0500

    minor change

--- day1/ans.py
+++ day1/ans.py
@@ -1,5 +1,5 @@
 class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
+    def singleNumber(self, nums: list[int]) -> int:
         ans = 0
         for num in nums:
             ans ^= num
